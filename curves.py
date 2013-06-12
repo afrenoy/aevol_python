3c528fbe3335d69c4a8a96ab1f1c0f28ddee5687
Antoine Frénoy <frenoy@gmx.com>
2013-06-12 19:56:10 +0200

    Can now be called directly and not only as a module. Will then take as arguments a list of experiments to treat.

--- curves.py
+++ curves.py
@@ -3,6 +3,24 @@
 import os
 import numpy
 from matplotlib.pyplot import *
+import sys
+
+def get_stats_exp(path):
+    allrep=[x for x in os.listdir(path) if os.path.isdir(os.path.join(path,x))]
+    nbrep=len(allrep)
+    nbgen=int(open(path + '/' + allrep[0] + '/last_gener.txt','r').read())+1
+    genomesize=numpy.zeros((nbrep,nbgen))
+    fitness=numpy.zeros((nbrep,nbgen))
+    metabolism=numpy.zeros((nbrep,nbgen))
+    secretion=numpy.zeros((nbrep,nbgen))
+    for rep,replicate in enumerate(allrep):
+        stat_fitness_glob=path + '/' + replicate + '/stats/stat_fitness_glob.out'
+        a=numpy.loadtxt(stat_fitness_glob)
+        genomesize[rep,:]=a[:,3]
+        fitness[rep,:]=a[:,2]
+        metabolism[rep,:]=a[:,6]
+        secretion[rep,:]=a[:,9]
+    return numpy.mean(genomesize,0),numpy.mean(fitness,0),numpy.mean(metabolism,0),numpy.mean(secretion,0),numpy.std(genomesize,0)/numpy.sqrt(nbrep),numpy.std(fitness,0)/numpy.sqrt(nbrep),numpy.std(metabolism,0)/numpy.sqrt(nbrep),numpy.std(secretion,0)/numpy.sqrt(nbrep)
 
 def output_stats(path):
     [g,f,m,s,g_s,f_s,m_s,s_s]=get_stats_exp(path)
@@ -28,19 +46,5 @@ def output_stats(path):
     fill_between(x,s-s_s,s+s_s,alpha=0.5,facecolor='blue',edgecolor='none')
     savefig(path+'/secretion.pdf')
 
-def get_stats_exp(path):
-    allrep=[x for x in os.listdir(path) if os.path.isdir(os.path.join(path,x))]
-    nbrep=len(allrep)
-    nbgen=int(open(path + '/' + allrep[0] + '/last_gener.txt','r').read())+1
-    genomesize=numpy.zeros((nbrep,nbgen))
-    fitness=numpy.zeros((nbrep,nbgen))
-    metabolism=numpy.zeros((nbrep,nbgen))
-    secretion=numpy.zeros((nbrep,nbgen))
-    for rep,replicate in enumerate(allrep):
-        stat_fitness_glob=path + '/' + replicate + '/stats/stat_fitness_glob.out'
-        a=numpy.loadtxt(stat_fitness_glob)
-        genomesize[rep,:]=a[:,3]
-        fitness[rep,:]=a[:,2]
-        metabolism[rep,:]=a[:,6]
-        secretion[rep,:]=a[:,9]
-    return numpy.mean(genomesize,0),numpy.mean(fitness,0),numpy.mean(metabolism,0),numpy.mean(secretion,0),numpy.std(genomesize,0)/numpy.sqrt(nbrep),numpy.std(fitness,0)/numpy.sqrt(nbrep),numpy.std(metabolism,0)/numpy.sqrt(nbrep),numpy.std(secretion,0)/numpy.sqrt(nbrep)
+for arg in sys.argv[1:]:
+    output_stats(arg)
