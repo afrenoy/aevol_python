6fa88d6b6954f95cda60494f6a00326cec7de65e
Antoine Frénoy <frenoy@gmx.com>
2014-03-13 12:15:50 +0100

    add other types of analysis

--- curves.py
+++ curves.py
@@ -204,6 +204,112 @@ def output_mean_std(path,start,end):
     savefig(path+'/sizenonfonctionalGenes.pdf')
     close()
 
+def output_all(path,start,end):
+    global genomesize
+    global fitness
+    global metabolism
+    global secretion
+    global cRNA
+    global ncRNA
+    global fgenes
+    global nfgenes
+    global sizecRNA
+    global sizencRNA
+    global sizefgenes
+    global sizenfgenes
+    
+    load_exp(path,start,end)
+    x=None
+    if start==None:
+        x=numpy.arange(0,numpy.shape(genomesize)[1],1)
+    elif end==None:
+        x=numpy.arange(start,start+numpy.shape(genomesize)[1],1)
+    else:
+        x=numpy.arange(start,end+1,1)
+    # genome size
+    figure()
+    plot(x.T,genomesize.T)
+    savefig(path+'/genomesize_all.pdf')
+    close()
+    # fitness
+    figure()
+    plot(x.T,fitness.T)
+    savefig(path+'/fitness_all.pdf')
+    close()
+    """
+    # metabolism
+    figure()
+    plot(x,m)
+    fill_between(x,m-m_s,m+m_s,alpha=0.5,facecolor='blue',edgecolor='none')
+    savefig(path+'/metabolism.pdf')
+    close()
+    # secretion
+    figure()
+    plot(x,s)
+    fill_between(x,s-s_s,s+s_s,alpha=0.5,facecolor='blue',edgecolor='none')
+    savefig(path+'/secretion.pdf')
+    close()
+    # coding RNA
+    figure()
+    plot(x,c)
+    fill_between(x,c-c_s,c+c_s,alpha=0.5,facecolor='blue',edgecolor='none')
+    savefig(path+'/codingRNA.pdf')
+    close()
+    # non coding RNA
+    figure()
+    plot(x,nc)
+    fill_between(x,nc-nc_s,nc+nc_s,alpha=0.5,facecolor='blue',edgecolor='none')
+    savefig(path+'/noncodingRNA.pdf')
+    close()
+    # fonctional genes
+    figure()
+    plot(x,fonc)
+    fill_between(x,fonc-fonc_s,fonc+fonc_s,alpha=0.5,facecolor='blue',edgecolor='none')
+    savefig(path+'/fonctionalGenes.pdf')
+    close()
+    # non fonctional genes
+    figure()
+    plot(x,nfonc)
+    fill_between(x,nfonc-nfonc_s,nfonc+nfonc_s,alpha=0.5,facecolor='blue',edgecolor='none')
+    savefig(path+'/nonfonctionalGenes.pdf')
+    close()
+    # size coding RNA
+    figure()
+    plot(x,sizec)
+    fill_between(x,sizec-sizec_s,sizec+sizec_s,alpha=0.5,facecolor='blue',edgecolor='none')
+    savefig(path+'/sizecodingRNA.pdf')
+    close()
+    # size non coding RNA
+    figure()
+    plot(x,sizenc)
+    fill_between(x,sizenc-sizenc_s,sizenc+sizenc_s,alpha=0.5,facecolor='blue',edgecolor='none')
+    savefig(path+'/sizenoncodingRNA.pdf')
+    close()
+    # size fonctional genes
+    figure()
+    plot(x,sizefonc)
+    fill_between(x,sizefonc-sizefonc_s,sizefonc+sizefonc_s,alpha=0.5,facecolor='blue',edgecolor='none')
+    savefig(path+'/sizefonctionalGenes.pdf')
+    close()
+    # size non fonctional genes
+    figure()
+    plot(x,sizenfonc)
+    fill_between(x,sizenfonc-sizenfonc_s,sizenfonc+sizenfonc_s,alpha=0.5,facecolor='blue',edgecolor='none')
+    savefig(path+'/sizenonfonctionalGenes.pdf')
+    close()
+    """
+
+def output_prctile(path,start,end):
+    load_exp(path,start,end)
+    x=None
+    if start==None:
+        x=numpy.arange(0,len(g),1)
+    elif end==None:
+        x=numpy.arange(start,start+len(g),1)
+    else:
+        x=numpy.arange(start,end+1,1)
+        
+        
 opts, args = getopt.getopt(sys.argv[1:], "s:e:", ["start", "end"])
 start=None
 end=None
@@ -215,4 +321,5 @@ for o, a in opts:
         end=int(a)
     
 for arg in args:
-    output_stats(arg,start,end)
+    #output_all(arg,start,end)
+    output_mean_std(arg,start,end)
